7cd46f1c8b2617251d84c1464a66662faaf34f35
Desenvolvimento15 <96912707+Desenvolvimento15@users.noreply.github.com>
2022-11-08 16:06:16 -0300

    vs01

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,3 +142,15 @@ if ($('#bodyConfirm').length) {
     }
    
    }
+
+
+   let mensagem = document.querySelector(".mensagem") ;
+
+// mostra a mensagem
+function showMessage(){   
+   mensagem.style.display = "block";   
+ }
+// esconde a mensagem
+function hideMessage(){
+  mensagem.style.display = "none"; 
+}
